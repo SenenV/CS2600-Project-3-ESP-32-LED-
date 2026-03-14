e1fec28f57488eea5ec1fd5493132ace9d27e0e9
Senen Velasco <senenvelasco@cpp.edu>
2026-03-13 21:44:26 -0700

    ESP32 external LED Blink (button)

---
+++ esp32-led-w-button.ino
@@ -0,0 +1,20 @@
+#define PIN_LED 2
+#define PIN_BUTTON 13
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(PIN_BUTTON)==HIGH){
+    digitalWrite(PIN_LED, LOW);
+  }
+  else{
+    digitalWrite(PIN_LED, HIGH);
+  }
+
+}
